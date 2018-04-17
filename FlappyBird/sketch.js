19b31710e7316a22f1b54af086479988ddfe6d13
stejakub <37673855+stejakub@users.noreply.github.com>
2018-04-17 17:38:47 +0200

    Initial commit

---
+++ FlappyBird/sketch.js
@@ -0,0 +1,38 @@
+var bird;
+var pipes = [];
+
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird();
+  pipes.push(new Pipe());
+}
+
+function draw() {
+  background(1);
+  bird.update();
+  bird.show();
+
+  if(frameCount % 80 == 0){
+  	pipes.push(new Pipe());
+  }
+
+  for (var i = pipes.length - 1; i >=0; i--) {
+  	pipes[i].show();
+  	pipes[i].update();
+
+    if(pipes[i].hits(bird)){
+      console.log("Hit");
+    }
+
+    if(pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
+}
+
+function keyPressed(){
+	if(key == ' '){	
+		bird.up();
+		console.log("SPACE");
+	}
+}
